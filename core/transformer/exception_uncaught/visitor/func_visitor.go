1068910546972a4eb437d4bd2d0c64477dfcbc18
dataznGao <datazngao@gmail.com>
2023-05-09 22:01:18 +0800

    fix: 修复空指针

--- core/transformer/exception_uncaught/visitor/func_visitor.go
+++ core/transformer/exception_uncaught/visitor/func_visitor.go
@@ -25,44 +25,46 @@ func (v *ExceptionUncaughtFuncVisitor) Visit(node ast.Node) ast.Visitor {
 		}
 		can := transformer.FunCanInjure(v.File, v.lp, structs, decl.Name.Name)
 		if can {
-			// 对函数段中不同的stmt进行单独处理
-			n := len(decl.Body.List)
-			for i := n - 1; i >= 0; i-- {
-				stmt := decl.Body.List[i]
-				if ifStmt, ok := stmt.(*ast.IfStmt); ok {
-					visitor := &ExceptionUncaughtIfVisitor{
-						lp:   v.lp,
-						File: v.File,
-					}
-					ast.Walk(visitor, ifStmt)
-					if visitor.can {
-						lo := fmt.Sprintf("[bingo] INFO 变异位置: %v\n%v\n", v.File.FileName, util.GetNodeCode(decl))
-						replica := util.CopyStmtList(decl.Body.List)
-						if i == len(decl.Body.List)-1 {
-							decl.Body.List = decl.Body.List[0:i]
-						} else {
-							decl.Body.List = append(decl.Body.List[0:i], decl.Body.List[i+1:]...)
+			if decl.Body != nil && decl.Body.List != nil {
+				// 对函数段中不同的stmt进行单独处理
+				n := len(decl.Body.List)
+				for i := n - 1; i >= 0; i-- {
+					stmt := decl.Body.List[i]
+					if ifStmt, ok := stmt.(*ast.IfStmt); ok {
+						visitor := &ExceptionUncaughtIfVisitor{
+							lp:   v.lp,
+							File: v.File,
 						}
-						if newPath, has := transformer.HasRunError(v.File); has {
-							decl.Body.List = replica
-							transformer.CreateFile(v.File)
-						} else {
-							log.Printf(lo)
-							log.Printf("[bingo] INFO 变异位置: %v\n变异为: \n%v\n", newPath, util.GetNodeCode(decl))
+						ast.Walk(visitor, ifStmt)
+						if visitor.can {
+							lo := fmt.Sprintf("[bingo] INFO 变异位置: %v\n%v\n", v.File.FileName, util.GetNodeCode(decl))
+							replica := util.CopyStmtList(decl.Body.List)
+							if i == len(decl.Body.List)-1 {
+								decl.Body.List = decl.Body.List[0:i]
+							} else {
+								decl.Body.List = append(decl.Body.List[0:i], decl.Body.List[i+1:]...)
+							}
+							if newPath, has := transformer.HasRunError(v.File); has {
+								decl.Body.List = replica
+								transformer.CreateFile(v.File)
+							} else {
+								log.Printf(lo)
+								log.Printf("[bingo] INFO 变异位置: %v\n变异为: \n%v\n", newPath, util.GetNodeCode(decl))
+							}
 						}
+					} else if caseStmt, ok := stmt.(*ast.SwitchStmt); ok {
+						visitor := &ExceptionUncaughtIfVisitor{
+							lp:   v.lp,
+							File: v.File,
+						}
+						ast.Walk(visitor, caseStmt)
+					} else if asignStmt, ok := stmt.(*ast.AssignStmt); ok {
+						visitor := &ExceptionUncaughtAssignVisitor{
+							lp:   v.lp,
+							File: v.File,
+						}
+						ast.Walk(visitor, asignStmt)
 					}
-				} else if caseStmt, ok := stmt.(*ast.SwitchStmt); ok {
-					visitor := &ExceptionUncaughtIfVisitor{
-						lp:   v.lp,
-						File: v.File,
-					}
-					ast.Walk(visitor, caseStmt)
-				} else if asignStmt, ok := stmt.(*ast.AssignStmt); ok {
-					visitor := &ExceptionUncaughtAssignVisitor{
-						lp:   v.lp,
-						File: v.File,
-					}
-					ast.Walk(visitor, asignStmt)
 				}
 			}
 		}
--- core/transformer/sync/visitor/func_visitor.go
+++ core/transformer/sync/visitor/func_visitor.go
@@ -25,47 +25,49 @@ func (v *SyncFuncVisitor) Visit(node ast.Node) ast.Visitor {
 		}
 		can := transformer.FunCanInjure(v.File, v.lp, structs, decl.Name.Name)
 		if can {
-			// 对函数段中不同的stmt进行单独处理
 			if decl.Body != nil && decl.Body.List != nil {
-				for i, stmt := range decl.Body.List {
-					if ifStmt, ok := stmt.(*ast.IfStmt); ok {
-						visitor := &SyncIfVisitor{
-							lp:   v.lp,
-							can:  false,
-							File: v.File,
+				// 对函数段中不同的stmt进行单独处理
+				if decl.Body != nil && decl.Body.List != nil {
+					for i, stmt := range decl.Body.List {
+						if ifStmt, ok := stmt.(*ast.IfStmt); ok {
+							visitor := &SyncIfVisitor{
+								lp:   v.lp,
+								can:  false,
+								File: v.File,
+							}
+							ast.Walk(visitor, ifStmt)
+						} else if goStmt, ok := stmt.(*ast.GoStmt); ok {
+							visitor := &SyncGoVisitor{
+								lp:   v.lp,
+								call: nil,
+								File: v.File,
+							}
+							ast.Walk(visitor, goStmt)
+							lo := fmt.Sprintf("[bingo] INFO 变异位置: %v\n%v\n", v.File.FileName, util.GetNodeCode(stmt))
+							var expr = new(ast.ExprStmt)
+							expr.X = visitor.call
+							rep := decl.Body.List[i]
+							decl.Body.List[i] = expr
+							if newPath, has := transformer.HasRunError(v.File); has {
+								decl.Body.List[i] = rep
+								transformer.CreateFile(v.File)
+							} else {
+								log.Printf(lo)
+								log.Printf("[bingo] INFO 变异位置: %v\n变异为: \n%v\n", newPath, util.GetNodeCode(expr))
+							}
+						} else if forStmt, ok := stmt.(*ast.ForStmt); ok {
+							visitor := &SyncForVisitor{
+								lp:   v.lp,
+								File: v.File,
+							}
+							ast.Walk(visitor, forStmt)
+						} else if caseStmt, ok := stmt.(*ast.SwitchStmt); ok {
+							visitor := &SyncCaseVisitor{
+								lp:   v.lp,
+								File: v.File,
+							}
+							ast.Walk(visitor, caseStmt)
 						}
-						ast.Walk(visitor, ifStmt)
-					} else if goStmt, ok := stmt.(*ast.GoStmt); ok {
-						visitor := &SyncGoVisitor{
-							lp:   v.lp,
-							call: nil,
-							File: v.File,
-						}
-						ast.Walk(visitor, goStmt)
-						lo := fmt.Sprintf("[bingo] INFO 变异位置: %v\n%v\n", v.File.FileName, util.GetNodeCode(stmt))
-						var expr = new(ast.ExprStmt)
-						expr.X = visitor.call
-						rep := decl.Body.List[i]
-						decl.Body.List[i] = expr
-						if newPath, has := transformer.HasRunError(v.File); has {
-							decl.Body.List[i] = rep
-							transformer.CreateFile(v.File)
-						} else {
-							log.Printf(lo)
-							log.Printf("[bingo] INFO 变异位置: %v\n变异为: \n%v\n", newPath, util.GetNodeCode(expr))
-						}
-					} else if forStmt, ok := stmt.(*ast.ForStmt); ok {
-						visitor := &SyncForVisitor{
-							lp:   v.lp,
-							File: v.File,
-						}
-						ast.Walk(visitor, forStmt)
-					} else if caseStmt, ok := stmt.(*ast.SwitchStmt); ok {
-						visitor := &SyncCaseVisitor{
-							lp:   v.lp,
-							File: v.File,
-						}
-						ast.Walk(visitor, caseStmt)
 					}
 				}
 			}
