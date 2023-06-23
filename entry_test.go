68d6c2ce8bc56adbfb4d0ea1b44c1d3b94b494f9
dataznGao <datazngao@gmail.com>
2023-06-24 01:02:23 +0800

    feat: 先获取依赖

--- entry_test.go
+++ entry_test.go
@@ -6,15 +6,15 @@ func TestFaultPerformerFactory_Run(t *testing.T) {
 	env := CreateMutationEnv("/Users/misery/GolandProjects/jupiter",
 		"/Users/misery/GolandProjects/errr",
 		"/Users/misery/GolandProjects/jupiter/test/e2e")
-	// env.ConditionInversedFault("util(1/5).myStruct(1/3).myFunc(1/2).myVariable | mmap(3/4).*.*.a(3/4)")
-	//env.NullFault("*.*.*.a")
-	//env.ConditionInversedFault("*.*.*.*")
-	//env.ExceptionUncaughtFault("*.*.*.*")
-	//env.ExceptionUncaughtFault("*.*.*.*")
-	//env.ExceptionUnhandledFault("*.*.*.*")
-	//env.SwitchMissDefaultFault("scene.*.*.a")
-	//env.ValueFault("*.*.*.id", "\"str\"")
-	//env.AttributeReversoFault("*.*.*.c", 10)
+	env.ConditionInversedFault("util(1/5).myStruct(1/3).myFunc(1/2).myVariable | mmap(3/4).*.*.a(3/4)")
+	env.NullFault("*.*.*.a")
+	env.ConditionInversedFault("*.*.*.*")
+	env.ExceptionUncaughtFault("*.*.*.*")
+	env.ExceptionUncaughtFault("*.*.*.*")
+	env.ExceptionUnhandledFault("*.*.*.*")
+	env.SwitchMissDefaultFault("scene.*.*.a")
+	env.ValueFault("*.*.*.id", "\"str\"")
+	env.AttributeReversoFault("*.*.*.c", 10)
 
 	env.SyncFault("*.*.*.*")
 	f := MutationPerformer{}
